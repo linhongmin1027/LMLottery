29eb391a49e1c8f5e9cd64fd7d44afc6c3876eaf
林宏敏 <543038188@qq.com>
2017-10-10 13:14:40 +0800

    liheng

---
+++ LMLottery/Main/LihengViewController.m
@@ -0,0 +1,37 @@
+//
+//  LihengViewController.m
+//  LMLottery
+//
+//  Created by 林宏敏 on 2017/10/10.
+//  Copyright © 2017年 李恒. All rights reserved.
+//
+
+#import "LihengViewController.h"
+
+@interface LihengViewController ()
+
+@end
+
+@implementation LihengViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"liheng");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
