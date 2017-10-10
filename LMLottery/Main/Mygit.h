b5cb9d539c47120575c91ba08d342fc3817cd3f7
林宏敏 <543038188@qq.com>
2017-10-10 14:46:30 +0800

    qe

---
+++ LMLottery/Main/Mygit.h
@@ -0,0 +1,13 @@
+//
+//  Mygit.h
+//  LMLottery
+//
+//  Created by 林宏敏 on 2017/10/10.
+//  Copyright © 2017年 李恒. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Mygit : NSObject
+
+@end
---
+++ LMLottery/Main/Mygit.m
@@ -0,0 +1,20 @@
+//
+//  Mygit.m
+//  LMLottery
+//
+//  Created by 林宏敏 on 2017/10/10.
+//  Copyright © 2017年 李恒. All rights reserved.
+//
+
+#import "Mygit.h"
+
+@implementation Mygit
+-(instancetype)init{
+    
+    self=[super init];
+    NSLog(@"myGit");
+    
+    return self;
+    
+}
+@end
